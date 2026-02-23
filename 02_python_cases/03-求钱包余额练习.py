427a71a5abc24cfcd6124de1e9e40a7819009b07
zimu-java-python <2869758975@qq.com>
2026-02-23 20:29:27 +0800

    Create 03-求钱包余额练习.py

---
+++ 02_python_cases/03-求钱包余额练习.py
@@ -0,0 +1,16 @@
+# 初始的钱包余额，50元
+money = 50
+print("当前钱包余额：", money, "元")
+
+
+# 购买了冰淇淋10元
+# 购买了可乐5元
+
+icecream = 10
+coke = 5
+print("购买了冰淇淋，花费：", icecream ,"元")
+print("购买了可乐，花费：", coke,"元")
+
+money = money - 10
+money = money - 5
+print("最终，钱包剩余：", money ,"元")

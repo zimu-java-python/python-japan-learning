1a7ab3986538cc97d505276e63543d48bea72a4c
zimu-java-python <2869758975@qq.com>
2026-02-24 21:49:53 +0800

    新增2个python课程文件：数据容器入门和列表的定义

---
+++ 01_python_basic/38-列表的定义语法.py
@@ -0,0 +1,24 @@
+"""
+列表的定义格式
+"""
+
+"""
+列表内的每一个数据，称之为“元素”
+以[]作为标识
+列表内的每一个元素之间，用逗号隔开
+"""
+
+# 定义一个列表 list
+my_list = ["itheima","itcast","python"]
+print(my_list)
+print(type(my_list))
+
+my_list = ["itheima",666,True]
+print(my_list)
+print(type(my_list))
+
+# 定义一个嵌套的列表
+my_list = [[1,2,3],[4,5,6]]
+print(my_list)
+print(type(my_list))
+
